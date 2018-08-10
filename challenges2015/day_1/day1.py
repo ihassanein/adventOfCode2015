9d52f309412f77faad43558c479b983f781c0474
Iman <iman.hassanein@merinio.com>
2018-08-10 12:48:31 -0400

    Day 1 complete

--- challenges2015/day_1/day1.py
+++ challenges2015/day_1/day1.py
@@ -2,18 +2,20 @@
 # Started August 2018
 # Coded in Python
 
+# Initialize floor variable #
 floor = 0
+counter = 0
 
+# Open input file to read #
 with open("input.txt", "r") as data:
     while True:
         move = data.read(1)
         if move == '(':
             floor += 1
+            counter += 1
         elif move == ')':
             floor -= 1
-
-        if not move:
-            print "End of File"
+            counter += 1
+        if floor < 0:
             break
-
-    print "You would be located on floor", floor
+    print counter
